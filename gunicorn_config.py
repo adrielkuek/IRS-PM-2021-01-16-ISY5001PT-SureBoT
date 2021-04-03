3110441d1671a7c80923ba79eb48d56ed749195d
Lavanya-IS <e0687387@u.nus.edu>
2021-04-03 11:01:52 +0800

    Committing modified Gunicorn config file

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,12 +1,13 @@
+preload_app = True
 bind = '0.0.0.0:5000'
 backlog = 2048
 daemon = True
 
 workers = 1
-worker_class = 'sync'
-worker_connections = 1000
-timeout = 30
-keepalive = 2
+# worker_class = 'sync'
+# worker_connections = 1000
+# timeout = 30
+# keepalive = 2
 
 errorlog = 'error.log'
 loglevel = 'debug'
