eab7cff48e6d3d3f8ac50116fc73d2233e9302b8
Lavanya-IS <e0687387@u.nus.edu>
2021-04-05 17:36:39 +0800

    Try Catch block for Pipeline Exception handling

--- SureBoT_main.py
+++ SureBoT_main.py
@@ -55,102 +55,105 @@ def executePipeline(query):
     #####################################################
     # Initialization
     #####################################################
-    start = time.time()
-    cwd = os.path.dirname(os.path.realpath(__file__))
-    print(f'INITIALISE EVIDENCE RETRIEVAL PIPELINE . . .')
-    ER_pipeline = EvidenceRetrieval(cwd, device)
+    try:
+        start = time.time()
+        cwd = os.path.dirname(os.path.realpath(__file__))
+        print(f'INITIALISE EVIDENCE RETRIEVAL PIPELINE . . .')
+        ER_pipeline = EvidenceRetrieval(cwd, device)
 
-    output_message = "*FACT-CHECK:* " + query
-    ################# SAMPLE QUERIES/URLS #####################
-    # query = "A bus driver has been arrested for careless driving following an accident at Loyang Avenue that killed a 31-year-old cyclist."
-    # query = "I will be charged for sending Whatsapp Good morning messages"
-    # query = "Alabama nurse in the states has just had the vaccine and she died 8 hours later"
-    # query = "https://www.straitstimes.com/singapore/bus-driver-arrested-for-careless-driving-after-cyclist-31-pronounced-dead-in-loyang?utm_source=Telegram&utm_medium=Social&utm_campaign=STTG"
-    # query = "https://www.theonlinecitizen.com/2020/07/03/10-mil-population-debacle-sdp-questions-why-former-dpm-heng-did-not-refute-st-report-at-the-time-it-was-published/"
-    # query = "https://newnaratif.com/podcast/an-interview-with-dr-paul-tambyah/"
-    # query = "https://www.straitstimes.com/tech/tech-news/whatsapp-delays-data-sharing-change-after-backlash-sees-users-flock-to-rivals"
+        output_message = "*FACT-CHECK:* " + query
+        ################# SAMPLE QUERIES/URLS #####################
+        # query = "A bus driver has been arrested for careless driving following an accident at Loyang Avenue that killed a 31-year-old cyclist."
+        # query = "I will be charged for sending Whatsapp Good morning messages"
+        # query = "Alabama nurse in the states has just had the vaccine and she died 8 hours later"
+        # query = "https://www.straitstimes.com/singapore/bus-driver-arrested-for-careless-driving-after-cyclist-31-pronounced-dead-in-loyang?utm_source=Telegram&utm_medium=Social&utm_campaign=STTG"
+        # query = "https://www.theonlinecitizen.com/2020/07/03/10-mil-population-debacle-sdp-questions-why-former-dpm-heng-did-not-refute-st-report-at-the-time-it-was-published/"
+        # query = "https://newnaratif.com/podcast/an-interview-with-dr-paul-tambyah/"
+        # query = "https://www.straitstimes.com/tech/tech-news/whatsapp-delays-data-sharing-change-after-backlash-sees-users-flock-to-rivals"
 
-    # Commenting below statement because of UnicodeEncodeError when Emoji's are present
-    # print(f'INPUT QUERY: {query}')
+        # Commenting below statement because of UnicodeEncodeError when Emoji's are present
+        # print(f'INPUT QUERY: {query}')
 
-    # Check URL Validity
-    '''
-    headers = {
-        'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.163 Safari/535.1'}
-    query_urlstatus = validators.url(query)
-    if query_urlstatus == True:
-        querytext = fulltext(requests.get(query, headers=headers).text)
-    else:
-        querytext = query
-    '''
-    # Query Preprocessing
-    querytext = query_preprocessing(query)
-
-    # Use SPACY to get number of tokens
-    nlp = English()
-    myDoc = nlp(querytext)
-    sentenceToken = []
-    for token in myDoc:
-        sentenceToken.append(token.text)
-
-    print(f'TOTAL NO. OF TOKENS FROM QUERY: {len(sentenceToken)}')
-    print(sentenceToken)
-
-    # If tokens > 50 - Perform Abstractive Summary on Query
-    # Else just skip and perform Doc Retrieval
-    if len(sentenceToken) > 50:
-        querytext = ER_pipeline.AbstractiveSummary(querytext, length_penalty)
-
-    # Run ER pipeline
-    start_time = time.time()
-    Filtered_Articles = []
-    Filtered_Articles = ER_pipeline.RetrieveArticles(querytext, topN)
-    print(f'>>>>>>> TIME TAKEN - ER PIPELINE: {time.time() - start_time}')
-
-    print(len(Filtered_Articles))
-    if len(Filtered_Articles) == 0:
-        output_message += '\n\nNO MATCHING ARTICLES FOUND. NOT ENOUGH EVIDENCE!'
-        print(f'NO MATCHING ARTICLES FOUND. NOT ENOUGH EVIDENCE!')
-    else:
-        # Run Fact Verification - Graph NET
-        graphNet = graphNetFC(cwd, device, feature_num, evidence_num, graph_layers,
-                              num_class, graph_pool, sequence_length)
-
-        FactVerification_List = []
-        for i in range(len(Filtered_Articles)):
-            pred_dict, outputs, heatmap = graphNet.predict(querytext, Filtered_Articles[i][1])
-
-            FactVerification_List.append(pred_dict['predicted_label'])
-            print(pred_dict)
-            print('[SUPPORTS, REFUTES, NOT ENOUGH INFO]')
-            print((np.array(outputs)))
-
-            # Plot Attention Heat map to visualize
-            ax = sns.heatmap(heatmap, linewidth=1.0, cmap="YlGnBu")
-            plt.show()
-            plt.clf()
-
-        maj_vote = 0
-        for i in range(len(Filtered_Articles)):
-            print(f'ARTICLE: {Filtered_Articles[i][2]} - {FactVerification_List[i]}')
-            if FactVerification_List[i] == 'SUPPORTS':
-                maj_vote += 1
-
-        if (maj_vote / len(Filtered_Articles)) > 0.6:
-            final_score = 'SUPPORTS'
-            print(f'************** FINAL SCORE: SUPPORTS')
-        elif (maj_vote / len(Filtered_Articles)) == 0.5:
-            final_score = 'NOT ENOUGH EVIDENCE'
-            print(f'************** FINAL SCORE: NOT ENOUGH SUPPORTING EVIDENCE')
+        # Check URL Validity
+        '''
+        headers = {
+            'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.163 Safari/535.1'}
+        query_urlstatus = validators.url(query)
+        if query_urlstatus == True:
+            querytext = fulltext(requests.get(query, headers=headers).text)
         else:
-            final_score = 'REFUTES'
-            print(f'************** FINAL SCORE: REFUTES')
+            querytext = query
+        '''
+        # Query Preprocessing
+        querytext = query_preprocessing(query)
 
-        output_message += '\n\n*FINAL SCORE:* ' + final_score
-        for i in range(len(Filtered_Articles)):
-            output_message += '\n\n\n' + Filtered_Articles[i][2] + '\n\n'
-            for j in range(len(Filtered_Articles[i][1])):
-                output_message += Filtered_Articles[i][1][j]
+        # Use SPACY to get number of tokens
+        nlp = English()
+        myDoc = nlp(querytext)
+        sentenceToken = []
+        for token in myDoc:
+            sentenceToken.append(token.text)
+
+        print(f'TOTAL NO. OF TOKENS FROM QUERY: {len(sentenceToken)}')
+        print(sentenceToken)
+
+        # If tokens > 50 - Perform Abstractive Summary on Query
+        # Else just skip and perform Doc Retrieval
+        if len(sentenceToken) > 50:
+            querytext = ER_pipeline.AbstractiveSummary(querytext, length_penalty)
+
+        # Run ER pipeline
+        start_time = time.time()
+        Filtered_Articles = []
+        Filtered_Articles = ER_pipeline.RetrieveArticles(querytext, topN)
+        print(f'>>>>>>> TIME TAKEN - ER PIPELINE: {time.time() - start_time}')
+
+        print(len(Filtered_Articles))
+        if len(Filtered_Articles) == 0:
+            output_message += '\n\nNO MATCHING ARTICLES FOUND. NOT ENOUGH EVIDENCE!'
+            print(f'NO MATCHING ARTICLES FOUND. NOT ENOUGH EVIDENCE!')
+        else:
+            # Run Fact Verification - Graph NET
+            graphNet = graphNetFC(cwd, device, feature_num, evidence_num, graph_layers,
+                                  num_class, graph_pool, sequence_length)
+
+            FactVerification_List = []
+            for i in range(len(Filtered_Articles)):
+                pred_dict, outputs, heatmap = graphNet.predict(querytext, Filtered_Articles[i][1])
+
+                FactVerification_List.append(pred_dict['predicted_label'])
+                print(pred_dict)
+                print('[SUPPORTS, REFUTES, NOT ENOUGH INFO]')
+                print((np.array(outputs)))
+
+                # Plot Attention Heat map to visualize
+                # ax = sns.heatmap(heatmap, linewidth=1.0, cmap="YlGnBu")
+                # plt.show()
+                # plt.clf()
+
+            maj_vote = 0
+            for i in range(len(Filtered_Articles)):
+                print(f'ARTICLE: {Filtered_Articles[i][2]} - {FactVerification_List[i]}')
+                if FactVerification_List[i] == 'SUPPORTS':
+                    maj_vote += 1
+
+            if (maj_vote / len(Filtered_Articles)) > 0.6:
+                final_score = 'SUPPORTS'
+                print(f'************** FINAL SCORE: SUPPORTS')
+            elif (maj_vote / len(Filtered_Articles)) == 0.5:
+                final_score = 'NOT ENOUGH EVIDENCE'
+                print(f'************** FINAL SCORE: NOT ENOUGH SUPPORTING EVIDENCE')
+            else:
+                final_score = 'REFUTES'
+                print(f'************** FINAL SCORE: REFUTES')
+
+            output_message += '\n\n*FINAL SCORE:* ' + final_score
+            for i in range(len(Filtered_Articles)):
+                output_message += '\n\n\n' + Filtered_Articles[i][2] + '\n\n'
+                for j in range(len(Filtered_Articles[i][1])):
+                    output_message += Filtered_Articles[i][1][j]
+    except:
+        output_message = 'Exception occurred in pipeline'
 
     return output_message
 
@@ -189,7 +192,7 @@ def query_preprocessing(query):
 
 
 if __name__ == "__main__":
-    text = "\ud83d\udc4d\ud83d\ude4c()Indeed,  Vivian Balakrishnan is a world class leader and politician. Establish Singapore very well on the world map.\ud83d\udc4f\ud83e\udd42\n\nhttps://newnaratif.com/podcast/an-interview-with-dr-paul-tambyah/"
+    text = "https://www.straitstimes.com/singapore/all-adult-sporeans-to-get-100-tourism-vouchers-in-december-for-staycations-attractions-and"
     result = executePipeline(text)
     result = result.encode('utf-16', 'surrogatepass').decode('utf-16')
     print('Result is: ' + result)
