d745ea2e7370ab61e1a05fb5b1be79d22f8be87b
Lavanya-IS <e0687387@u.nus.edu>
2021-04-06 12:03:12 +0800

    Configuration for Supervisor to run Gunicorn and celery as daemon processes

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ timeout = 100
 model_download_url = 'https://drive.google.com/uc?id=1aY7ipPeI_-RgONIocO9FUYDdHcTG5ak6'
 model_zip = 'pipeline_models.zip'
 model_folder = '/pipeline_models'
-CELERY_RESULT_BACKEND = 'redis://localhost'
-CELERY_BROKER_URL = 'redis://localhost'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
